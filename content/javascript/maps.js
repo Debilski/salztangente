da763c7d8ae53168c95dad3333611a41b4ade84b
Rike-Benjamin Schuppner <rikebs@debilski.de>
2015-04-14 08:58:53 +0200

    dump

---
+++ content/javascript/maps.js
@@ -0,0 +1,109 @@
+$(function() {
+        var setLocation = function(location) {
+                $('#slider-aside h1').html(location);
+        }
+
+        var setCoords = function(coords) {
+                // change this
+                var maps = "https://www.google.de/maps/place/";
+                //
+
+                $('#slider-aside p').html('<a href="' + maps + encodeURIComponent(coords) + '" target="_blank">' + coords + '</a>');
+        }
+
+        var setAdditional = function(additional, thumb) {
+                if (additional) {
+                        additional = additional.split(",");
+                        if (thumb == undefined) thumb = additional;
+
+                        var htmlCode = ""
+                        $.each(additional, function(idx, elem) {
+                                htmlCode += '<a href="..' + elem + '"><img src="..' + elem + '.thmb.jpg" width="120" /></a><br>';
+                        });
+                        $('#slider-aside .slider-aside-image').html("<div>" + htmlCode + "</div>");
+                } else {
+                        $('#slider-aside .slider-aside-image div').remove();
+                }
+        }
+
+        function showImage(id) {
+                // get DOM for the image
+                var image = $('.carousel .item[data-imgid=' + id + ']').first();
+
+                var idx = $('.carousel .item').index(image);
+
+                var thumbIndex = $(image).data("refid");
+                var location = $(image).data("location");
+                var additional = $(image).data("additional");
+                var additional_thumb = $(image).data("additional_thumb");
+                var coords = $(image).data("coordinates");
+                var pdf = $(image).data("pdf");
+                var pdf_size = $(image).data("pdf_size");
+
+                if (!location) return true;
+                if (location) setLocation(location);
+
+                setCoords(coords)
+                setAdditional(additional, additional_thumb);
+
+                if (thumbIndex == undefined || isNaN(thumbIndex)) return true;
+
+                // call the carousel
+                $('.carousel').carousel(idx);
+                document.location.hash = "/" + (thumbIndex);
+
+                return false;
+        }
+
+        // assign a click event to the external thumbnails
+        $('.slider-thumbs a').click(function(){
+                var thumbIndex = $(this).data("refid");
+                return showImage(thumbIndex);
+        });
+
+        function checkHash() {
+                if($(location).attr("hash")) {
+                        var hash = $(location).attr("hash");
+                        var site_id_rgx = /#\/(\d+)/;
+                        var site_id = parseInt(site_id_rgx.exec(hash)[1]);
+
+                        return showImage(site_id);
+                } else {
+                        return showImage(0);
+                }
+        }
+
+        $('.carousel').on('slide.bs.carousel', function(direction, relatedTarget) {
+        });
+        $(window).on('hashchange', checkHash);
+        //        $('.carousel .item').first().addClass("active");
+
+        if($(location).attr("hash")) {
+                var hash = $(location).attr("hash");
+                var site_id_rgx = /#\/(\d+)/;
+                var site_id = parseInt(site_id_rgx.exec(hash)[1]);
+
+                var image = $('.carousel .item[data-imgid=' + site_id + ']').first();
+
+                var idx = $('.carousel .item').index(image);
+                $('.carousel .item:eq('+ idx + ')').addClass("active");
+        }
+
+        checkHash();
+
+/*
+   $(".carousel").swiperight(function() {
+      $(this).carousel('prev');
+      showImage($('.carousel .active').data('imgid'));
+    });
+   $(".carousel").swipeleft(function() {
+      $(this).carousel('next');
+      showImage($('.carousel .active').data('imgid'));
+   });
+*/
+});
+
+
+        function showAdditional() {
+                window.open("/salztangente" + $(".item.active").first().data("additional"));
+        }
